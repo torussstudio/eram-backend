bb5051221e18035d8f9e4ee493e301c04090a8a7
muhammed-asharaf-vp <mohamedasharafvpp@gmail.com>
2026-06-23 11:51:22 +0530

    Update CORS for production

--- controllers/authController.js
+++ controllers/authController.js
@@ -69,7 +69,7 @@ export const loginAdmin = async (req, res) => {
     res.cookie("token", token, {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
@@ -93,7 +93,7 @@ export const logoutAdmin = async (req, res) => {
   res.cookie("token", "", {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  sameSite: "none",
   expires: new Date(0),
 });
 
